3aa5a0b4a97258aeb7cb0bce6c0c6a366f990666
OppositeNor <3248161469@qq.com>
2023-03-28 21:01:38 +0800

    add dependencies for other system

--- include/cos_graphics/defs.h
+++ include/cos_graphics/defs.h
@@ -7,4 +7,33 @@ typedef char CG_BOOL;
 
 #define CG_TRUE 1
 #define CG_FALSE 0
+
+// define target platform
+#ifdef __WIN32__
+    #define CG_TG_WIN              //windows
+    #ifdef _WIN649
+        #define CG_TG_WIN_64       //windows 64
+    #else
+        #define CG_TG_WIN_32       //windows 32
+    #endif // _WIN64
+#elif __APPLE__
+    #include "TargetConditionals.h"
+    #if TARGET_IPHONE_SIMULATOR
+        #error "IPhone Simulator is not supported for CosGraphics"
+    #elif TARGET_OS_IPHONE
+        #error "IPhone is not supported for CosGraphics"
+    #elif TARGET_OS_MAC
+        #define CG_TG_MACOS        //mac os
+    #else
+        #error "Unknown target platform"
+    #endif
+#elif __linux__
+    #define CG_TG_LINUX              //linux
+#elif __unix__
+    #error "Unix is not supported for CosGraphics"
+#elif __ANDROID__
+    #error "Android is not supported for CosGraphics"
+#else
+    #error "Unknown target platform"
+#endif
 #endif
