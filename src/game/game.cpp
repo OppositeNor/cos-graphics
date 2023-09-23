607681b90fa4026f4154f5f9d54e7cbcbbe5e89f
OppositeNor <68041679+OppositeNor@users.noreply.github.com>
2023-09-23 18:22:09 -0400

    update

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -28,7 +28,7 @@ void CGGame::InitGame(unsigned int p_width, unsigned int p_height, CGString p_ti
         delete game_instance;
         game_instance = nullptr;
     }
-    CGGame::game_instance = CreateGameInstance();
+    CGGame::game_instance = new CGGame();
     CGGame::game_instance->window_properties = WindowProperties(p_width, p_height, p_title, p_fullscreen, 
         p_resizable, p_boarderless, p_transparent, p_topmost);
     CG_PRINT(CGSTR("Creating window..."));
